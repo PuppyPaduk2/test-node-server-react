1551c68171903d6cf2a615e179f32b7b02508fdc
puppypaduk2 <debraooclark@gmail.com>
2022-01-12 11:27:10 +0300

    setup: Add babel-loader

--- webpack/common.js
+++ webpack/common.js
@@ -1,8 +1,23 @@
+const path = require("path");
+
 module.exports = {
   mode: "production",
   entry: undefined,
   output: undefined,
   externals: undefined,
-  module: undefined,
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            configFile: path.resolve(__dirname, "./babel.config.js"),
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: undefined,
 };
